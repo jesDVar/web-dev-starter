049f2563e94fad266853525e8584d626fa0c6dea
Jesse Varis <jessevaris@hotmail.com>
2022-01-13 16:55:30 +0100

    Playing around

---
+++ script.js
@@ -0,0 +1,11 @@
+if (1 == '1')  {
+    console.log('true');
+}
+    else  {
+        console.log('false');
+    }
+    let price = 100;
+
+    let message = (price > 10) ? 'expensive' : 'cheap';
+    console.log(message);
+    console.log(price);
