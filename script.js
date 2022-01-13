9d4a0d81d553a4e0163e897cbe4356302bac54c0
Jesse Varis <jessevaris@hotmail.com>
2022-01-13 21:14:50 +0100

    Save

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
-if (1 == '1')  {
-    console.log('true');
-}
-    else  {
-        console.log('false');
-    }
-    let price = 100;
+// if (1 == '1')  {
+//     console.log('true');
+// }
+//     else  {
+//         console.log('false');
+//     }
+//     let price = 100;
 
-    let message = (price > 10) ? 'expensive' : 'cheap';
+//     let message = (price > 10) ? 'expensive' : 'cheap';
+//     console.log(message);
+//     console.log(price);
+
+
+// function logMessage()
+// {
+//     console.log('heres a messagw');
+// }
+// logMessage();
+
+let myFunction = function loggingFunction(message, firstName) {
     console.log(message);
-    console.log(price);
+    console.log(firstName);
+}
+
+myFunction('Hello', 'Steve');
+// console.log(typeof(message));
